d6840daaf065b2c49f4e6004548f967dfe142921
li-zi-hang <15537174991@163.com>
2022-09-26 13:53:02 +0800

    update

---
+++ intehel-demo/src/main/java/com/intehel/demo/XXLApplication.java
@@ -0,0 +1,13 @@
+package com.intehel.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XXLApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XXLApplication.class, args);
+    }
+
+}
